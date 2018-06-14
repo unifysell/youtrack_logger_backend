5e3b89e4531627fd9cdd1c0b4918b775eaf1edc5
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2018-06-14 12:28:47 +0200

    creating  integration test: logging to youtrack backend once

--- test/logger/backends/log_test.exs
+++ test/logger/backends/log_test.exs
@@ -1,9 +1,38 @@
 defmodule Logger.Backends.LogTest do
   use ExUnit.Case, async: true
+  use Plug.Test
 
   require Logger
 
-  test "can log error" do
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  test "can log error", %{bypass: bypass} do
+    Bypass.expect_once(bypass, "PUT", "/rest/issue", fn conn ->
+      Plug.Conn.resp(conn, 200, "")
+    end)
+
+    Application.put_env(
+      :logger,
+      :youtrack,
+      host: "http://localhost:#{bypass.port}",
+      token: "XXX",
+      project: "Sandbox",
+      level: :warn
+    )
+
+    Logger.add_backend({Logger.Backends.Youtrack, :error})
+
     :ok = Logger.error("testing error log")
+
+    Logger.remove_backend({Logger.Backends.Youtrack, :error})
+
+    Application.put_env(
+      :logger,
+      :youtrack,
+      nil
+    )
   end
 end
