a95ac2d64e5de34faae73a7e59ee0eefcaed0cf8
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2018-06-14 12:36:39 +0200

    refactored test

--- test/logger/backends/log_test.exs
+++ test/logger/backends/log_test.exs
@@ -1,18 +1,11 @@
 defmodule Logger.Backends.LogTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
   use Plug.Test
 
   require Logger
 
   setup do
     bypass = Bypass.open()
-    {:ok, bypass: bypass}
-  end
-
-  test "can log error", %{bypass: bypass} do
-    Bypass.expect_once(bypass, "PUT", "/rest/issue", fn conn ->
-      Plug.Conn.resp(conn, 200, "")
-    end)
 
     Application.put_env(
       :logger,
@@ -25,14 +18,26 @@ defmodule Logger.Backends.LogTest do
 
     Logger.add_backend({Logger.Backends.Youtrack, :error})
 
+    on_exit(fn ->
+      Logger.remove_backend({Logger.Backends.Youtrack, :error})
+
+      Application.put_env(
+        :logger,
+        :youtrack,
+        nil
+      )
+
+      :ok
+    end)
+
+    {:ok, bypass: bypass}
+  end
+
+  test "can log error", %{bypass: bypass} do
+    Bypass.expect_once(bypass, "PUT", "/rest/issue", fn conn ->
+      Plug.Conn.resp(conn, 200, "")
+    end)
+
     :ok = Logger.error("testing error log")
-
-    Logger.remove_backend({Logger.Backends.Youtrack, :error})
-
-    Application.put_env(
-      :logger,
-      :youtrack,
-      nil
-    )
   end
 end
